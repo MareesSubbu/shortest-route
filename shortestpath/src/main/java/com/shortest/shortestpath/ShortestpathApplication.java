f22ea40a3e9c23f58f406fe3ddd0d4b9e7defb11
mareeswaran.s <mareeswaran.s@TVMATP307631D.ad.infosys.com>
2018-11-15 18:41:47 +0530

    Initial file creation

--- shortestpath/src/main/java/com/shortest/shortestpath/ShortestpathApplication.java
+++ shortestpath/src/main/java/com/shortest/shortestpath/ShortestpathApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShortestpathApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ShortestpathApplication.class, args);
 	}
