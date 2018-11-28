ab05fa4980da3c74f597130a6f63a9b7c39be346
MareesSubbu <45069955+MareesSubbu@users.noreply.github.com>
2018-11-28 12:19:46 +0530

    proper sql version added

--- vagrant/db.sh
+++ vagrant/db.sh
@@ -2,8 +2,10 @@
 # Packages
 BUILD_ESSENTIAL="build-essential"
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+  debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+  debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
   apt-get update
-  apt-get install -y mysql-server-5.5.0
+  apt-get install -y mysql-server-5.5
 
 
 
